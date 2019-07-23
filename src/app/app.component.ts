45fbd2060c548d8b4d80af720818da7d557ed1dc
ImsirovicAjdin <ajdika@live.com>
2019-07-23 05:34:21 +0200

    The code for building a nested component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,62 @@ export class AppComponent  {
 Building Nested Components
 ################################
 
+There are 2 ways to use a component and display a component's template. We can use a component as a directive:
+<body>
+  <pm-root></pm-root>
+</body>
+
+Alternatively, we can use a component as a routing target, so that it appears to the user that they've travelled to another view.
+
+The template is then displayed in a full-page style view.
+
+What makes a component nestable?
+
+- Its template only manages a fragment of a larger view
+- It has a selector
+- It optionally communicates with its container
+
+We'll cover:
+- Using a Nested Component
+- Passing Data to a nested component using @Input
+- Raising an event from a nested component using @output
+
+
 
 Building a Nested Component
+###########################
 Here is a visual representation of a component that is nestable. Here is another component. It wants to use the nestable component in its template. We then refer to the outer component as the container or parent component, and we refer to the inner component as the nested, or child component. When building an interactive application, the nested component often needs to communicate with its container. The nested component receives information from its container using input properties, and the nested component outputs information back to its container by raising events. In our sample application, we want to change the display of the 5 Star Rating from this, to this. Displaying the rating number using a visual representation such as stars makes it quicker and easier for the user to interpret the meaning of the number. This is the nested component we'll build in this module. For the star component to display the correct number of stars, the container must provide the rating number to our star component as an input. And if the user clicks on the stars, we want to raise an event to notify the container. Let's jump right in and build our star component. When we last saw our sample application, we had completed the product list component. Now of course, we want to change it. Instead of displaying a number for the rating here, we want to display stars. Instead of adding a code to the product list component to display the stars, we want to build it as a separate component. This keeps the template and logic for that feature encapsulated and makes it reusable. So let's begin by creating a star component. The star component can be used by any feature of the application, so it really doesn't belong in our products folder. We'll instead put it in a shared folder where we'll put all our shared components. If you are using the starter files, I've already created this folder and included the template and style sheet for our component here. Now we are ready to build the star component. We begin by creating a new file. We'll name it star. component. ts. We then create this component just like we'd create any other component, starting with the class, export class StarComponent. What's next? Yep, we decorate the class with the Component decorator. Recall that it is this Component decorator that makes this class a component. As always, it shows us a syntax error here because we are missing, yep, our import. Time to set the Component decorator properties. For the selector, we'll set pm-star. For the templateUrl, we provide the path to the HTML file provided with the starter files. We'll add the styleUrls property, and in the array we'll set the first element to the path of the style sheet that was also provided. Since both files are in the same folder as the component, we can use relative pathing. Now let's take a peek at the star component template. Here it displays five stars. It then crops the stars based on a defined width. This technique can then display partial stars, such as four and a half of the five stars by setting the width such that only four and a half of the stars appear. Recall what this syntax is called? This is property binding. We're using it here to set the style width and here to bind the title property to display the numeric rating value. For these bindings to work, we need two properties in the components class, the rating and the starWidth. Let's add these two properties. We want a rating property, which is a number and defines the rating value. Since we don't yet have a way to get this value, let's hardcode it to 4 for now so we'll see some stars. And we need the starWidth. This value is calculated based on the rating. So where do we put that calculation? Well, we'd want the starWidth recalculated any time the container changed the rating number. So let's tap into the OnChanges lifecycle hook as we discussed in the last module. We'll implement the OnChanges interface, and we'll write code for the ngOnChanges method identified in the OnChanges interface. In this method, we'll convert the rating number to a starWidth based on the width of our stars. Our component is now complete, and we're ready to nest it in another component. How do we do that?
 
+// star.component.ts:
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+  rating: number =4;
+  starWidth: number;
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
+}
+
+// star.component.html:
+<div class="crop"
+  [style.width.px]="starWidth"
+  [title]="rating">
+<div style="width: 75px">
+  <span class="fa fa-star"></span>
+  <span class="fa fa-star"></span>
+  <span class="fa fa-star"></span>
+  <span class="fa fa-star"></span>
+</div>
+</div>
+
+Using a Nested Component as a directive
+##########################################
 
 /*
